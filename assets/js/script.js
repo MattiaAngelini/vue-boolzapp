648a87cd167916a6b7474705d9f58c7a448250bd
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-03 14:55:00 +0200

    Init project

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Milestone 1
+// - Replica della grafica con la possibilità di avere messaggi scritti dall’utente 
+// (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+
+// - Visualizzazione dinamica della lista contatti: 
+// tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+// Milestone 2
+// - Visualizzazione dinamica dei messaggi: tramite la direttiva v-for,
+//  visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+
+//  - Click sul contatto mostra la conversazione del contatto cliccato
+// Trovate in allegato lo starter pack con immagini, screenshot e il file js con la struttura dati.
+
+
+
+
+let matti = 10;
+console.log(matti)
+
