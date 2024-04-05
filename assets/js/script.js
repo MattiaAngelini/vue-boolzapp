3c4790a351bd5b583432d0ebdd9b1e6f7c3d0a81
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-05 21:48:38 +0200

    Add: indentation - MILESTONE 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,145 +1,129 @@
 const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-          userActive: 0,
-          userMessageValue: '',
-            contacts: [{
-                name: 'Michele',
-                avatar: '_1',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Hai portato a spasso il cane?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Ricordati di dargli da mangiare',
-                    status: 'sent'
-                  },
-                  {
-                    date: '10/01/2020 16:15:22',
-                    message: 'Tutto fatto!',
-                    status: 'received'
-                  }
-                ],
-              },  
-              {
-                name: 'Fabio',
-                avatar: '_2',
-                visible: true,
-                messages: [{
-                    date: '20/03/2020 16:30:00',
-                    message: 'Ciao come stai?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '20/03/2020 16:30:55',
-                    message: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
-                  },
-                  {
-                    date: '20/03/2020 16:35:00',
-                    message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'received'
-                  }
-                ],
-              },
-              {
-                name: 'Samuele',
-                avatar: '_3',
-                visible: true,
-                messages: [{
-                    date: '28/03/2020 10:10:40',
-                    message: 'La Marianna va in campagna',
-                    status: 'received'
-                  },
-                  {
-                    date: '28/03/2020 10:20:10',
-                    message: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '28/03/2020 16:15:22',
-                    message: 'Ah scusa!',
-                    status: 'received'
-                  }
-                ],
-              },
-              {
-                name: 'Luisa',
-                avatar: '_4',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
-                  }
-                ],
-              },
-            ],
-        };
-    },
-    methods: {
-      
-		accountActive(index){
-			this.userActive = index;
+  data() {
+    return {
+      userActive: 0,
+      userMessageValue: '',
+      contacts: [{
+          name: 'Michele',
+          avatar: '_1',
+          visible: true,
+          messages: [{
+              date: '10/01/2020 15:30:55',
+              message: 'Hai portato a spasso il cane?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Ricordati di dargli da mangiare',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 16:15:22',
+              message: 'Tutto fatto!',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Fabio',
+          avatar: '_2',
+          visible: true,
+          messages: [{
+              date: '20/03/2020 16:30:00',
+              message: 'Ciao come stai?',
+              status: 'sent'
+            },
+            {
+              date: '20/03/2020 16:30:55',
+              message: 'Bene grazie! Stasera ci vediamo?',
+              status: 'received'
+            },
+            {
+              date: '20/03/2020 16:35:00',
+              message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Samuele',
+          avatar: '_3',
+          visible: true,
+          messages: [{
+              date: '28/03/2020 10:10:40',
+              message: 'La Marianna va in campagna',
+              status: 'received'
+            },
+            {
+              date: '28/03/2020 10:20:10',
+              message: 'Sicuro di non aver sbagliato chat?',
+              status: 'sent'
+            },
+            {
+              date: '28/03/2020 16:15:22',
+              message: 'Ah scusa!',
+              status: 'received'
+            }
+          ],
+        },
+        {
+          name: 'Luisa',
+          avatar: '_4',
+          visible: true,
+          messages: [{
+              date: '10/01/2020 15:30:55',
+              message: 'Lo sai che ha aperto una nuova pizzeria?',
+              status: 'sent'
+            },
+            {
+              date: '10/01/2020 15:50:00',
+              message: 'Si, ma preferirei andare al cinema',
+              status: 'received'
+            }
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    accountActive(index) {
+      this.userActive = index;
       console.log(this.userActive)
-		},
+    },
     sendMessage() {
       // Aggiungiamo il messaggio solo se è stato digitato qualcosa, 
-      //quindi diverso da nulla
+      // quindi diverso da nulla
       if (this.userMessageValue !== '') {
-          //creo costante per nuovo messaggio inserito 
-          //che conterrà data corrente, valore inserito nella input,
-          //e status sent per attribuzione classe
+        // Creo costante per nuovo messaggio inserito 
+        // che conterrà data corrente, valore inserito nella input,
+        // e status sent per attribuzione classe
         const newMessage = {
-              date: new Date().toLocaleString(),
-              message: this.userMessageValue,
-              status: 'sent'
+          date: new Date().toLocaleString(),
+          message: this.userMessageValue,
+          status: 'sent'
+        };
+        // Aggiungiamo il nuovo messaggio alla lista dei messaggi del contatto attivo
+        this.contacts[this.userActive].messages.push(newMessage);
+
+        // Dopo aver inviato messaggio, nella stessa funzione 3 secondi dopo,
+        // faccio comparire messaggio di risposta con 'ok'
+        setTimeout(() => {
+          const replyMessage = {
+            date: new Date().toLocaleString(),
+            message: 'ok',
+            status: 'received'
           };
-              // Aggiungiamo il nuovo messaggio alla lista dei messaggi del contatto attivo
-              this.contacts[this.userActive].messages.push(newMessage);
-                 
-          //Dopo aver inviato messaggio, nella stessa funzione 3 secondi dopo,
-          //faccio comparrire messaggio di risposta con 'ok'
-          setTimeout(() => {
-            const replyMessage = {
-              date: new Date().toLocaleString(),
-              message: 'ok',
-              status: 'received'
-            };
-             // Aggiungiamo risposta alla lista dei messaggi del contatto attivo
-             this.contacts[this.userActive].messages.push(replyMessage);
+          // Aggiungiamo risposta alla lista dei messaggi del contatto attivo
+          this.contacts[this.userActive].messages.push(replyMessage);
 
-          //3 secondi di intervallo
-          },3000);
-       
-          // Reset della barra di input dopo l'invio del messaggio
-          this.userMessageValue = '';
-          
+          // 3 secondi di intervallo
+        }, 3000);
+
+        // Reset della barra di input dopo l'invio del messaggio
+        this.userMessageValue = '';
       }
-  },
-
-    }
+    },
+  }
 }).mount('#app');
-
-// Milestone 3
-// - Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa
-//  e digitando “enter” il testo viene aggiunto al thread sopra, 
-//  come messaggio verde
-
-//  - Risposta dall’interlocutore: ad ogni inserimento di un messaggio,
-//   l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
-
-//   Milestone 4
-// - Ricerca utenti: scrivendo qualcosa nell’input a sinistra,
-//  vengono visualizzati solo i contatti il cui nome contiene le lettere inserite 
-// (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
